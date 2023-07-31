5dfc5ac0c26fe214a1142046a8373be012d351b3
gajjarvatsall <vatsalgajjar84@gmail.com>
2023-07-31 15:34:57 +0530

    - UI Completed

--- lib/modules/home_Screen.dart
+++ lib/modules/home_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:ai_chat_bot/constant/pallete.dart';
+import 'package:ai_chat_bot/widgets/feature_box.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,53 +13,107 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("J A R V I S"),
-          leading: const Icon(Icons.menu),
-        ),
-        body: Column(
-          children: [
-            Stack(
-              children: [
-                Center(
-                  child: Container(
-                    height: 120,
-                    width: 120,
-                    margin: const EdgeInsets.only(top: 4),
-                    decoration: const BoxDecoration(
-                        color: Pallete.assistantCircleColor,
-                        shape: BoxShape.circle),
-                  ),
-                ),
-                Container(
-                  height: 123,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("J A R V I S"),
+        leading: const Icon(Icons.menu),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 4),
                   decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/virtualAssistant.png"),
-                    ),
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Container(
+                height: 123,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/virtualAssistant.png"),
                   ),
                 ),
-              ],
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              margin: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ).copyWith(
-                top: 30,
-              ),
-              decoration: BoxDecoration(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 40,
+            ).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
                 border: Border.all(
                   color: Pallete.borderColor,
-                )
+                ),
+                borderRadius:
+                    BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                "Good Morning , What task can I do for you ?",
+                style: TextStyle(
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
+                    fontFamily: "CeraPro"),
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(
+              top: 10,
+              left: 22,
+            ),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              "Here are a few features",
+              style: TextStyle(
+                  fontFamily: "CeraPro",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Column(
+            children: [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: "ChatGPT",
+                descriptionText:
+                    "A smarter way to stay organized and informed ChatGPT",
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: "Dall-E",
+                descriptionText:
+                    "Get inspired and stay creative with your personal assistant powered by Dall-E",
+              ),
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: "Smart Voice Assistant",
+                descriptionText:
+                    "Get the best of both worlds with a voice assistant powered by ChatGPT and Dall-E",
+              ),
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        child: const Icon(Icons.mic),
+      ),
+    );
   }
 }
